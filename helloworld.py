c58af3cd4ead585633d288dd9cd0c244a142bb9e
VennelaAtmuri <vennela.atmuri@gmail.com>
2022-06-16 18:40:31 +0530

    changes made to the First Commit

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+print ("Hello World")
+name = input("Enter the name : ")
+print ("Hi",name)
+A = int(input("Enter the First Number A : "))
+B = int(input("Enter the Second Number B : "))
+C = A+B
+print ("Sum of ",A,"and ",B," = ",C)
+
